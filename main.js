dc2431253145e4e935acb23ded54c8f0e719b6ea
安斎亮 <anzaitoru@anzairyounoMacBook-Air.local>
2022-04-17 16:21:15 +0900

    電卓secondcommit

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 'use strict'
 
 let result = document.getElementById('result');
+let boal = true;
 
 function clickbutton(key) {
   let keyText = key.innerHTML;
-  if (keyText == "=") {
-    result.innerHTML = eval(result.innerHTML);
-    return;
-  } else if (keyText == "AC") {
-    result.innerHTML = "0";
-  } else if (result.innerHTML.slice(0) == "0") {
-    result.innerHTML = result.innerHTML.replace(result.innerHTML.slice(0), keyText);
+  if (boal == true) {
+    if (keyText == "=") {
+      result.innerHTML = eval(result.innerHTML);
+      boal = false;
+    } else if (keyText == "AC") {
+      result.innerHTML = "0";
+      boal = true;
+    } else if (result.innerHTML.slice(0) == "0") {
+      result.innerHTML = result.innerHTML.replace(result.innerHTML.slice(0), keyText);
+      boal = true;
+    } else {
+      result.innerHTML += keyText;
+      boal = true;
+    }
   } else {
-    result.innerHTML += keyText;
+    if (keyText == "=" || keyText == "AC") {
+      result.innerHTML = "0";
+      boal = true;
+    } else {
+      result.innerHTML = keyText;
+      boal = true;
+    } 
   }
 }
 
@@ -33,13 +47,16 @@ function clickPointButton(pointbutton) {
 }
 
 function clickZeroZerobutton(zerobutton) {
-  if (result.innerHTML.slice(0) == "0") {
-    result.innerHTML = 0;
-  } else {
-    result.innerHTML += zerobutton.innerHTML;
+  if (boal == true) {
+    if (result.innerHTML.slice(0) == "0") {
+      result.innerHTML = 0;
+    } else if (result.innerHTML.slice(-1) == "+" || result.innerHTML.slice(-1) == "-" || result.innerHTML.slice(-1) == "*" || result.innerHTML.slice(-1) == "/") {
+      result.innerHTML = result.innerHTML + "0";
+    } else {
+      result.innerHTML += zerobutton.innerHTML;
+    }
   }
 }
-
 
 
 
